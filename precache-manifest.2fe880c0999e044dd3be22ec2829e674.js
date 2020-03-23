aa7583e84708759b4bdc1e6b03b9b94371a0f8c7
bootrino <bootrino@gmail.com>
2020-03-23 15:53:37 +1100

    Initial commit

---
+++ precache-manifest.2fe880c0999e044dd3be22ec2829e674.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60b92faac44aa61588fa7e9f4ec5442c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c2719f83b637093e953",
+    "url": "/static/js/2.4603b22e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4603b22e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aff9094bc11d8d517ab",
+    "url": "/static/js/main.6f187ce9.chunk.js"
+  },
+  {
+    "revision": "0d6dd5f661a8d910732b",
+    "url": "/static/js/runtime-main.176a048f.js"
+  }
+]);
